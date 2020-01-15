060ec00004c05f71935b6a23bddff3ace71b0cf7
Didou <emma.bou_hanna@mines-paristech.fr>
2020-01-15 16:04:26 +0100

    Update bundle.py

--- needleman_wunsch/bundle.py
+++ needleman_wunsch/bundle.py
@@ -1,12 +1,9 @@
 from ruler import Ruler
-import csv
 from argparse import ArgumentParser
 parser = ArgumentParser()
 parser.add_argument("fichier", help= "le nom du fichier")
 args = parser.parse_args()
-import numpy as np
 
-'''1ère méthode : en utilisant l'itérateur'''
 
 with open(args.fichier, 'r') as f:
     sentinelle = True
@@ -35,28 +32,3 @@ with open(args.fichier, 'r') as f:
 
 
 print("fin de la comparaison")
-
-
-
-'''  Deuxième façon : avec pandas '''
-import pandas as pd
-
-with open(args.fichier, 'r') as fichier2:
-    dataset = pd.read_csv(fichier2, header=None) # peut lire un fichier .txt
-    n = len(dataset[0])
-    print(n)
-    print(dataset)
-    # Les lignes vides sont automatiquement supprimées par le pd.read_csv
-    # Mais au cas où, on remplace les lignes vides par des NaN et on les enlève
-
-    dataset.replace('', np.nan, inplace=True)
-    dataset.dropna(inplace=True)
-
-
-    for k in range(0, n//2):  # on ne prend pas en compte la dernière ligne
-        fragment = Ruler(dataset[0][2*k], dataset[0][2*k+1])
-        fragment.compute()
-        (top, bottom) = fragment.report()
-
-        print(
-            f"====== fragment #{k+1} - distance = {fragment.distance} \n{top}\n{bottom}")
